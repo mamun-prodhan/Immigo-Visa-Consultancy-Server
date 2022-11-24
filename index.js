d45fc1d72d08c11ee5819dce30aa00c021c5d960
mamun-prodhan <mamunpr9@gmail.com>
2022-11-24 22:16:48 +0600

    allservices added

--- index.js
+++ index.js
@@ -12,13 +12,43 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nhx4fnh.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('assignment11').collection('services');
+        const countryCollection = client.db('assignment11').collection('country');
+
+        app.get('/country', async(req, res) =>{
+            const query = {}
+            const cursor = countryCollection.find(query);
+            const country = await cursor.toArray();
+            res.send(country); 
+        })
+
+        app.get('/services', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query).limit(3);
+            // const cursor = serviceCollection.find(query,{"title":1,_id:0}).sort({"title":-1}).limit(3);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        
+        app.get('/allservices', async(req, res) =>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            // const cursor = serviceCollection.find(query,{"title":1,_id:0}).sort({"title":-1}).limit(3);
+            const allservices = await cursor.toArray();
+            res.send(allservices);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 
