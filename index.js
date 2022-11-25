bbac40c679105027523cd3ac60ed18d4c3edda7d
mamun-prodhan <mamunpr9@gmail.com>
2022-11-25 12:23:41 +0600

    get review from mongodb added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 // middlewares
 app.use(cors());
 app.use(express.json());
@@ -15,31 +16,31 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         const serviceCollection = client.db('assignment11').collection('services');
         const countryCollection = client.db('assignment11').collection('country');
         const reviewCollection = client.db('assignment11').collection('reviews');
 
         // for country section
-        app.get('/country', async(req, res) =>{
+        app.get('/country', async (req, res) => {
             const query = {}
             const cursor = countryCollection.find(query);
             const country = await cursor.toArray();
-            res.send(country); 
+            res.send(country);
         });
 
         // 3 service for home page
-        app.get('/services', async(req, res) =>{
+        app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query).limit(3);
             // const cursor = serviceCollection.find(query,{"title":1,_id:0}).sort({"title":-1}).limit(3);
             const services = await cursor.toArray();
             res.send(services);
         });
-        
+
         // all services for service page
-        app.get('/allservices', async(req, res) =>{
+        app.get('/allservices', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
             // const cursor = serviceCollection.find(query,{"title":1,_id:0}).sort({"title":-1}).limit(3);
@@ -48,17 +49,17 @@ async function run(){
         });
 
         // single service details page
-        app.get('/details/:id', async(req, res) => {
+        app.get('/details/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service);
         });
 
         // all reviews api
-        app.get('/reviews', async(req, res) =>{
+        app.get('/reviews', async (req, res) => {
             let query = {};
-            if(req.query.service){
+            if (req.query.service) {
                 query = {
                     service: req.query.service
                 }
@@ -68,14 +69,14 @@ async function run(){
             res.send(reviews);
         })
 
-        app.post('/reviews', async(req, res) =>{
+        app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         })
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -84,10 +85,10 @@ run().catch(err => console.error(err));
 
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('assignment 11 is running')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`assignment 11 is running on port ${port}`);
 })
