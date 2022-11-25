05eb54bb2098673a1e676b7f453d26737bf99577
mamun-prodhan <mamunpr9@gmail.com>
2022-11-25 23:26:18 +0600

    myreviews api added

--- index.js
+++ index.js
@@ -69,6 +69,19 @@ async function run() {
             res.send(reviews);
         })
 
+        // myreviews api==========
+        app.get('/myreviews', async(req, res) =>{
+            let query = {};
+            if(req.query.email){
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const myreviews = await cursor.toArray();
+            res.send(myreviews);
+        })
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
